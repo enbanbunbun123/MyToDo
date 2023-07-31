99e7ccf180dd7927ecec48581ff114cda3e09a4e
enbanbunbun123 <enbanbunbun-123@docomo.ne.jp>
2023-07-31 23:16:26 +0900

    footerコンポーネント追加

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+        <p>© 2023 YuhoTakahashi MyToDo</p>
+    </div>
+  );
+}
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./styles/index.css";
 import Header from "./components/Header";
 import Main from "./Main";
+import Footer from "./components/Footer";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +11,7 @@ ReactDOM.render(
     <div style={{ padding: "30px" }}>
       <Main />
     </div>
+    <Footer />
   </React.StrictMode>,
   document.getElementById("root"),
 );
