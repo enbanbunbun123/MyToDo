0f3b9084772222d6ce30e45a764f8107a48c1b5c
enbanbunbun123 <enbanbunbun-123@docomo.ne.jp>
2023-08-31 21:46:35 +0900

    fix: 追加エラーの文言を変更

--- src/components/CreateTask.js
+++ src/components/CreateTask.js
@@ -22,7 +22,7 @@ export default function CreateTask({ data, setdata }) {
     if(newTask === "") {
       setIsOpen(true);
       return;
-      }
+    }
 
     const newId = uuid4();
     const newDataTask = {
@@ -58,7 +58,7 @@ export default function CreateTask({ data, setdata }) {
         <div className="overlay">
           <div className="modal">
             <div className="modal-content">
-              <p className="modal-text">タスクの名前を入力してください</p>
+              <p className="modal-text">「タスクの名前」は入力必須項目です。</p>
               <button onClick={handleClose}>閉じる</button>
             </div>
           </div>
